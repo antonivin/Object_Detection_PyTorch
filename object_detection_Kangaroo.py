a7f925b61c21b33d139b8d2d08fb169b4465dfd7
antonivin <antonivin.ma@gmail.com>
2021-04-02 14:25:04 +0530

    Update object_detection_Kangaroo.py

--- object_detection_Kangaroo.py
+++ object_detection_Kangaroo.py
@@ -21,7 +21,7 @@ import torch.nn.functional as F
 import glob
 import os
 
-image_size = (64,64)
+image_size = (224,224)
 
 
 # In[2]:
@@ -30,212 +30,152 @@ image_size = (64,64)
 class kangaroo(Dataset):
     
     def __init__(self, path, transform):
-        image_dir = path + '/kangaroo/images' #path to images
-        annotation_dir = path + '/kangaroo/pascal'#path to annotation.xml files
-        self.transform = transform #transforms passed to the initializer
-        # list all the files in the folder, sort the files in alphabetical order and join the file name with folder name
-        # for image
+        image_dir = path + '/Object_Detection_PyTorch/kangaroo/images/' 
+        annotation_dir = path + '/Object_Detection_PyTorch/kangaroo/pascal/'
+        self.transform = transform 
         self.image_names = os.listdir(image_dir)
         self.image_names.sort()
         self.image_names = [os.path.join(image_dir,image_name) for image_name in self.image_names]
-        # for annotation.xml files
         self.annotation_names = os.listdir(annotation_dir)
         self.annotation_names.sort()
         self.annotation_names = [os.path.join(annotation_dir,annotation_name) for annotation_name in self.annotation_names]
-    
-    #create a custom dataset with labels   
+     
     def __getitem__(self, idx):
-        #load images from the folder
         image_name = self.image_names[idx]
-        img = Image.open(image_name) 
-        #load annotations from the folder
+        img = Image.open(image_name)
+        w,h = img.size
+        img = img.resize(image_size)
         annotation_name = self.annotation_names[idx]
         annotation_tree = ET.parse(annotation_name, ET.XMLParser(encoding='utf-8'))
-        classes = annotation_tree.find("object").find("name").text #parse the class labels
-        list_classes = [classes] #converting string to list
-        num_obj = len(list_classes) #read the number of labels
-        bndbox_xml = annotation_tree.find("object").find("bndbox") #parse the bounding box
+        classes = annotation_tree.find("object").find("name").text 
+        num_objs = 1
+        boxes = []
+        bndbox_xml = annotation_tree.find("object").find("bndbox")
         xmin = int(bndbox_xml.find('xmin').text)
         ymin = int(bndbox_xml.find('ymin').text)
         xmax = int(bndbox_xml.find('xmax').text)
         ymax = int(bndbox_xml.find('ymax').text)
-        bbox = (xmin, ymin, xmax, ymax)
-        img.crop(bbox) #crop the images with boundary box
-        if self.transform:
-            img = self.transform(img)  #apply the transforms to the image  
-        labels = torch.ones((num_obj), dtype=torch.int64) #convert the int label to tensor
-        return img, labels
+        boxes.append([xmin*224/w,ymin*224/h,xmax*224/w,ymax*224/h])
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        if self.transform is not None:
+            img, target = self.transform(img, target)
+        img /=255
+        return img, target
     
     def __len__(self):
         return len(self.image_names)
-        
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
 
 # In[3]:
 
 
-#mean and std from imagenet to normalize the images
-mean = [0.485, 0.456, 0.406]
-std  = [0.229, 0.224, 0.225]
-transform = transforms.Compose([
-                                transforms.Resize(image_size), #64*64
-                                transforms.ToTensor(), 
-                                transforms.Normalize(mean, std)])
-path = '/Users/nivin/Documents/Object Detection' #the path to the files
-dataset = kangaroo(path,transform = transform) # Initialize the dataset and transform each images to tensor
+import transforms as T
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    return T.Compose(transforms)
+
+
+# In[ ]:
+
+
+path = '/content' 
+dataset = kangaroo(path, transform = get_transform(True))
 print(len(dataset))
 
 
 # In[4]:
 
 
-#sample of images from the dataset
-def imshow(source):
-    plt.figure(figsize=(10,10))
-    imt = (source.view(-1, image_size[0], image_size[0]))
-    imt = imt.numpy().transpose([1,2,0])
-    imt = (std * imt + mean).clip(0,1)
-    plt.subplot(1,2,2)
-    plt.imshow(imt)
+train_set = int(0.8 * len(dataset)) 
+val_set = len(dataset) - train_set 
+print(train_set)
+print(val_set)
+batch_size = 3
+train_data, val_data = torch.utils.data.random_split(dataset, [train_set, val_set])
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True,collate_fn=collate_fn)
+validationloader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
 
 # In[5]:
 
 
-imshow(dataset[1][0])
-imshow(dataset[2][0])
-imshow(dataset[13][0])
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+num_classes = 2
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
 
 # In[6]:
 
 
-#split the dataset for training and validation
-train_set = int(0.8 * len(dataset)) # 80% for training
-val_set = len(dataset) - train_set # 20% for validation
-print(train_set)
-print(val_set)
-#Hyperparameter
-batch_size = 2
-train_data, val_data = torch.utils.data.random_split(dataset, [train_set, val_set]) #random split the data into non-overlapping datasets
-trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True) #configuring train loader : Batch size of 2 images
-validationloader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=True) #configuring validation loader :  Batch size of 2 images
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.0005, momentum=0.9, weight_decay=0.0005)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
 
 # In[7]:
 
 
-# the proposed network has two main convolutional layers and two fully connected linear layers, the number of nodes in layer 1
-# is 512 and the number of nodes in  layer 2 is 256. A dropout of 40% is used to avoid overfitting of dataset and a maxpooling
-# with 2x2 is used to reduce the dimension of the image and also to avoid overfitting
-import torch.optim as optim
-class Net(nn.Module): #create a class
-    def __init__(self): #initialise the class
-        super(Net, self).__init__()
-        #Main Layer 1
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 512, 5), 
-            nn.ReLU(),
-            nn.MaxPool2d(2,2))
-        #Main Layer 2
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(512, 256, 5),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2))
-        self.drop_out = nn.Dropout(p=0.40)
-        # Two fully connected layer
-        self.fc1 = nn.Linear(43264, 120)
-        self.fc2 = nn.Linear(120, 1)
-
-    def forward(self, x):
-        out = self.layer1(x) #pass through convolutional layers
-        out = self.layer2(out)
-        out = out.reshape(out.size(0), -1) #Flatten the output from convolutional layers to linear fully connected layers 
-        out = self.drop_out(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        return out
-    
-model = Net() #creating an instance for the model
-print(model)
+model.train()
+num_epochs = 5
+count = 0
+for i in range(num_epochs):
+  for img,targets in trainloader:
+    img = list(image for image in img)
+    target = [{k:v for k,v in t.items()} for t in targets]
+    loss_dict = model(img,targets)
+    loss = sum(loss for loss in loss_dict.values())
+    loss_val = loss.item()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    count = count + 1
+    if (count %10 == 0):
+      print("Loss at iter "+str(count)+":",loss_val)
+  print("Epoch "+str(i+1)+" Loss:",loss_val)
 
 
 # In[8]:
 
 
-#Hyperparameters
-epochs = 5
-lr = 0.0002
-momentum = 0.9
-weight_decay = 0.00001
-
-#Loss function BCEWithLogitsLoss that combines Sigmoid layer and the BCELoss in one single class. Binary Cross Entrophy
-# (BCE) calculates the binary entrophy between the target and output
-criterion = nn.BCEWithLogitsLoss()
-
-# Optimizer: stochastic gradient descent
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum = momentum, weight_decay = weight_decay)
+torch.save(model.state_dict(), 'kangaroo_fasterrcnn_resnet50.pth')
 
 
 # In[9]:
 
 
-train_loss = []
-testing_loss = []
-testaccuracy = []
-epochs = 5
-test_loss = 0
-period = 5
-def train(model, epochs):
-    for epoch in range(epochs):
-        train_loss = 0.0
-        count = 0
-        for idx, iteration in enumerate(trainloader):
-            count = count + 1
-            x, label = iteration
-            optimizer.zero_grad()# zero gradients of optimiser
-            pred_label = model(x)# forward propogation
-            loss = criterion(pred_label, label.float())     # compute loss 
-            loss.backward()     # find rate of change of loss with respect to model parameters - backward propogation
-            optimizer.step()#optimisation step -> optimise the weights and bias
-            train_loss += loss.item()*x.size(0)
-            if count % period == 0: # Print every 5 iterations
-                test_loss = 0
-                accuracy = 0
-                model.eval() #set to evaluation mode
-                with torch.no_grad():
-                    for idx,iteration in enumerate(validationloader):
-                        x, label = iteration
-                        pred_label = model(x)  #forward propogation
-                        val_loss = criterion(pred_label, label.float())# compute loss 
-                        test_loss += val_loss.item()*x.size(0)
-                train_loss = train_loss/period
-                test_loss = test_loss/len(validationloader)
-                testing_loss.append(test_loss)
-                #print the result
-                print('Epoch: {} \t Step: {} \tTrain Loss: {:.6f} \tTest loss: {:.6f}'.format(
-                        epoch+1,
-                        count,
-                        train_loss,
-                        test_loss,
-                        ))
-train(model, epochs)
+model.eval()
+img,targets = next(iter(validationloader))
+img = list(image for image in img)
+targets = [{k:v for k,v in t.items()} for t in targets]
+output = model(img)
+print(output)
 
 
 # In[10]:
 
 
-
-
-
-# In[11]:
-
-
-
-
-
-# In[ ]:
-
-
-
+from matplotlib import pyplot as plt
+fig, axs = plt.subplots(1, 1, figsize=(32, 16))
+#axs = axs.ravel()
+threshold = 0.5
+sample = img[0].permute(1,2,0).numpy()
+boxes = output[1]['boxes']
+scores = output[1]['scores']
+boxes = boxes[scores > threshold].astype(np.int32)
+res_img = ImageDraw.Draw(sample)
+for box in boxes:
+  res_img.rectangle([box[0], box[1], box[2], box[3]],outline = "Red",width = 1)
+axs[i].set_axis_off()
+axs[i].imshow(sample)
 
